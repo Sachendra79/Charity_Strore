7bef9aaea5d3259bea4f8b8cb2f2edf214e3f507
Somgupta786 <somvishwari786@gmail.com>
2024-01-27 22:42:35 +0530

    create upload screen

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,30 +1,28 @@
-import React, { useEffect,useState,createContext, useContext } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Login from './Components/Authentication/login/Login';
-import SignUp from './Components/Authentication/signUp/SignUp';
+import React, { useEffect, useState, createContext, useContext } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Login from "./Components/Authentication/login/Login";
+import SignUp from "./Components/Authentication/signUp/SignUp";
 import OtpPage from "./Components/Authentication/otp/otpPage";
-import Home from './Components/Pages/Home';
-import Forget from './Components/Authentication/forgetUsername/forget';
-import ProtectedRouter from './ProtectedRouter';
 
+import Forget from "./Components/Authentication/forgetUsername/forget";
+import ProtectedRouter from "./ProtectedRouter";
+import Faq from "./Components/Pages/Fq";
+import Upload from "./Components/Pages/upload";
+import UserSection from "./Components/Pages/userSection";
 
-export const loginContext=createContext("");
+export const loginContext = createContext("");
 function AppRouter() {
-
-    
   return (
-  
     <Routes>
-      <Route path="/" element={<SignUp/>} />
+      <Route path="/" element={<SignUp />} />
       <Route path="/login" element={<Login />} />
-      <Route path="/Otp" element={<OtpPage />}  />
-    
-    <Route path='/home' element={<Home/>}/>
-      <Route path="/forget" element={<Forget />} />
-  
+      <Route path="/Otp" element={<OtpPage />} />
 
+      <Route path="/forget" element={<Forget />} />
+      <Route path="/faq" element={<Faq />} />
+      <Route path="/upload" element={<Upload />} />
+      <Route path="/userSection" element={<UserSection />} />
     </Routes>
-   
   );
 }
 
