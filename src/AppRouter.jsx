eb0cd412575c3d430c32371884f074c88a1626db
Sachendra79 <sachendragangwar120@gmail.com>
2024-01-27 22:39:42 +0530

    completed home section

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -6,6 +6,7 @@ import OtpPage from "./Components/Authentication/otp/otpPage";
 import Home from './Components/Pages/Home';
 import Forget from './Components/Authentication/forgetUsername/forget';
 import ProtectedRouter from './ProtectedRouter';
+import About from './Components/Pages/About';
 
 
 export const loginContext=createContext("");
@@ -19,9 +20,9 @@ function AppRouter() {
       <Route path="/login" element={<Login />} />
       <Route path="/Otp" element={<OtpPage />}  />
     
-    <Route path='/home' element={<Home/>}/>
+      <Route path='/home' element={<Home/>}/>
       <Route path="/forget" element={<Forget />} />
-  
+      <Route path="/about" element={<About/>} />
 
     </Routes>
    
---
+++ src/Components/Pages/About.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      <div>
+        Navbar
+      </div>
+
+  <div >
+         
+        <div><h1>About Us</h1></div>
+       
+
+  </div>
+            
+
+    </div>
+  )
+}
+
+export default About
