d2c97daa39d8127b8f29febb75d3301ed6158563
Somgupta786 <somvishwari786@gmail.com>
2024-01-28 01:46:03 +0530

    create list page

--- src/Components/Pages/List.jsx
+++ src/Components/Pages/List.jsx
@@ -1,223 +1,77 @@
+ 
+import sofaa from "../../assets/sofaa.svg";
+import Logo from "../Authentication/LogoIcon";
+import axios from "axios";
+import { useState,useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+
 const ListPage = () => {
-    return (
-      <div className="w-full relative bg-lightgreen-100 h-[51.38rem] overflow-hidden text-left text-[2.5rem] text-white font-style-script">
-        <div className="absolute top-[calc(50%_-_297px)] left-[52.75rem] rounded-[20px] bg-yellowgreen shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] box-border w-[27.25rem] h-[40.25rem] border-[1px] border-solid border-dimgray" />
-        <div className="absolute top-[calc(50%_-_237px)] left-[56.38rem] flex flex-row items-start justify-start text-[1rem] font-web-para-reg-03">
-          <div className="flex flex-col items-center justify-center">
-            <div className="flex flex-col items-center justify-start gap-[1.5rem]">
-              <b className="relative text-[2rem] leading-[100%]">Welcome to</b>
-              <div className="w-[9rem] relative h-[2.5rem] text-[2.5rem] font-style-script">
-                <div className="absolute top-[0%] left-[0%] leading-[100%]">
-                  <span>Unity</span>
-                  <span className="text-olivedrab">Share</span>
-                </div>
-              </div>
-              <div className="w-[17.75rem] relative text-[1.25rem] leading-[150%] text-center inline-block">{`Sign Up to connect, collaborate and share ! `}</div>
-              <div className="w-[20rem] relative h-[4.5rem]">
-                <div className="absolute h-[77.78%] w-full top-[22.22%] right-[0%] bottom-[0%] left-[0%] rounded-3xl box-border border-[1px] border-solid border-white" />
-                <div className="absolute h-[44.44%] w-[17.5%] top-[0%] right-[76.72%] bottom-[55.56%] left-[5.78%] bg-yellowgreen flex flex-row items-center justify-center p-[0.5rem] box-border">
-                  <div className="relative leading-[100%]">Email</div>
-                </div>
-                <div className="absolute top-[50%] left-[6.88%] leading-[100%] text-gray-200">
-                  Enter Email
-                </div>
-              </div>
-              <div className="w-[20rem] relative h-[4.5rem]">
-                <div className="absolute h-[77.78%] w-full top-[22.22%] right-[0%] bottom-[0%] left-[0%] rounded-3xl box-border border-[1px] border-solid border-white" />
-                <div className="absolute h-[44.44%] w-[17.5%] top-[0%] right-[75.31%] bottom-[55.56%] left-[7.19%] bg-yellowgreen flex flex-row items-center justify-center p-[0.5rem] box-border">
-                  <div className="relative leading-[100%]">Email</div>
-                </div>
-                <div className="absolute h-[77.78%] w-full top-[22.22%] right-[0%] bottom-[0%] left-[0%] rounded-3xl box-border border-[1px] border-solid border-white" />
-                <div className="absolute h-[44.44%] w-[27.5%] top-[0%] right-[67.19%] bottom-[55.56%] left-[5.31%] bg-yellowgreen flex flex-row items-center justify-center p-[0.5rem] box-border">
-                  <div className="relative leading-[100%]">Password</div>
-                </div>
-                <div className="absolute top-[50%] left-[6.88%] leading-[100%] text-gray-200">
-                  Enter Password
-                </div>
-              </div>
-              <div className="flex flex-row items-center justify-start text-center">
-                <img
-                  className="w-[1.13rem] relative rounded-8xs h-[1.13rem]"
-                  alt=""
-                  src="/check-box.svg"
-                />
-                <div className="w-[18.56rem] relative leading-[150%] inline-block shrink-0">
-                  By creating account you agree to accept our Privacy Policy.
-                </div>
-              </div>
-              <div className="w-[20rem] rounded-3xl bg-olivedrab flex flex-row items-center justify-center py-[1rem] px-[6.63rem] box-border text-[1.25rem]">
-                <div className="relative leading-[100%]">Continue</div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="absolute top-[1.5rem] left-[10rem] flex flex-row items-center justify-start gap-[60.13rem]">
-          <div className="w-[6.44rem] relative h-[2.5rem]">
-            <div className="absolute top-[0%] left-[0%] leading-[100%]">
-              <span>Unity</span>
-              <span className="text-olivedrab">Share</span>
-            </div>
-          </div>
-          <b className="relative text-[1.25rem] leading-[100%] font-web-para-reg-03 text-olivedrab">
-            Login
-          </b>
-        </div>
-        <div className="absolute h-[67.33%] w-[41.85%] top-[20.92%] right-[47.04%] bottom-[11.74%] left-[11.11%]">
-          <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%]">
-            <div className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%]">
-              <div className="absolute h-[84.33%] w-full top-[7.23%] right-[0%] bottom-[8.45%] left-[0%]">
-                <img
-                  className="absolute h-[97.05%] w-full top-[2.95%] right-[0%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full"
-                  alt=""
-                  src="/vector.svg"
-                />
-                <div className="absolute h-[98.04%] w-[75.93%] top-[0%] right-[22.14%] bottom-[1.96%] left-[1.93%]">
-                  <img
-                    className="absolute h-full w-full top-[0%] right-[0%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full"
-                    alt=""
-                    src="/group.svg"
-                  />
-                  <div className="absolute h-[89.19%] w-[77.75%] top-[5.41%] right-[11.12%] bottom-[5.4%] left-[11.13%]">
-                    <div className="absolute h-[23.31%] w-[99.88%] top-[0%] right-[0.03%] bottom-[76.69%] left-[0.1%]">
-                      <img
-                        className="absolute h-[36.62%] w-full top-[63.38%] right-[0%] bottom-[0%] left-[0%] max-w-full overflow-hidden max-h-full"
-                        alt=""
-                        src="/group.svg"
-                      />
-                      <img
-                        className="absolute h-[18.17%] w-[58.48%] top-[0%] right-[20.72%] bottom-[81.83%] left-[20.8%] max-w-full overflow-hidden max-h-full"
-                        alt=""
-                        src="/group.svg"
-                      />
-                    </div>
-                    <div className="absolute h-[23.32%] w-full top-[76.68%] right-[0%] bottom-[0%] left-[0%]">
-                      <img
-                        className="absolute h-[36.86%] w-full top-[0%] right-[0%] bottom-[63.14%] left-[0%] max-w-full overflow-hidden max-h-full"
-                        alt=""
-                        src="/group.svg"
-                      />
-                      <img
-                        className="absolute h-[18.23%] w-[58.33%] top-[81.77%] right-[20.91%] bottom-[0%] left-[20.76%] max-w-full overflow-hidden max-h-full"
-                        alt=""
-                        src="/group.svg"
-                      />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <img
-                className="absolute h-full w-[82.8%] top-[0%] right-[8.6%] bottom-[0%] left-[8.6%] max-w-full overflow-hidden max-h-full"
-                alt=""
-                src="/group.svg"
-              />
-            </div>
-          </div>
-        </div>
-        <div className="absolute top-[-0.06rem] left-[0rem] w-[90rem] h-[51.38rem] text-[1.5rem] text-midnightblue font-inter">
-          <div className="absolute top-[0rem] left-[0rem] bg-whitesmoke w-[90rem] h-[51.38rem] overflow-hidden">
-            <div className="absolute top-[4.5rem] left-[0rem] bg-gainsboro w-[90rem] h-[4.5rem] hidden" />
-            <div className="absolute top-[0rem] left-[0rem] bg-lightgoldenrodyellow shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[90rem] h-[5rem]" />
-            <div className="absolute top-[0.75rem] left-[calc(50%_-_282px)] rounded-31xl bg-lightgreen-100 shadow-[0px_4px_2px_rgba(0,_0,_0,_0.15)] w-[35.25rem] h-[3.5rem]" />
-            <div className="absolute top-[1.75rem] left-[29.25rem] leading-[100%] font-web-para-reg-03 text-gray-300">
-              Search here
-            </div>
-            <div className="absolute top-[5.19rem] left-[0rem] bg-lightgreen-200 w-[90rem] h-[4rem]" />
-            <div className="absolute top-[5.44rem] left-[68.25rem] rounded-31xl bg-whitesmoke shadow-[0px_4px_2px_rgba(0,_0,_0,_0.15)] w-[11.75rem] overflow-hidden flex flex-row items-center justify-center py-[0.75rem] px-[4.63rem] box-border gap-[0.63rem] text-black font-web-para-reg-03">
-              <div className="relative leading-[100%]">Category</div>
-              <img
-                className="w-[2rem] relative h-[2rem] overflow-hidden shrink-0 object-contain"
-                alt=""
-                src="/dashiconsarrowup@2x.png"
-              />
-            </div>
-            <img
-              className="absolute top-[6.19rem] left-[10rem] w-[2rem] h-[2rem] overflow-hidden object-contain"
-              alt=""
-              src="/codiconarrowup@2x.png"
-            />
-            <div className="absolute top-[6.44rem] left-[12.69rem] leading-[100%] font-web-para-reg-03 text-gray-100">
-              Back
-            </div>
-            <div className="absolute top-[25rem] left-[11rem] leading-[100%]">
-              Brown Sofa
-            </div>
-            <div className="absolute top-[25rem] left-[28.69rem] leading-[100%]">
-              Red Handbag
-            </div>
-            <div className="absolute top-[25rem] left-[47.13rem] leading-[100%]">
-              Pikachu
-            </div>
-            <div className="absolute top-[25rem] left-[64.44rem] leading-[100%]">
-              Milk and honey novel
-            </div>
-          </div>
-          <div className="absolute top-[10.44rem] left-[10rem] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[70rem] h-[17.69rem]">
-            <div className="absolute top-[0rem] left-[0rem] bg-lightgreen-300 w-[16.33rem] h-[17.69rem]" />
-            <div className="absolute top-[0rem] left-[17.87rem] bg-lightgreen-300 w-[16.39rem] h-[17.69rem]" />
-            <img
-              className="absolute top-[0.53rem] left-[0.89rem] w-[14.79rem] h-[12.84rem] object-cover"
-              alt=""
-              src="/rectangle-26@2x.png"
-            />
-            <div className="absolute top-[0rem] left-[35.8rem] bg-lightgreen-300 w-[16.33rem] h-[17.69rem]" />
-            <div className="absolute top-[0rem] left-[53.67rem] bg-lightgreen-300 w-[16.33rem] h-[17.69rem]" />
-            <img
-              className="absolute top-[0.53rem] left-[18.82rem] w-[14.79rem] h-[12.84rem] object-cover"
-              alt=""
-              src="/rectangle-27@2x.png"
-            />
-            <img
-              className="absolute top-[0.53rem] left-[37.22rem] w-[14.79rem] h-[12.84rem] object-cover"
-              alt=""
-              src="/rectangle-28@2x.png"
-            />
-            <img
-              className="absolute top-[0.53rem] left-[54.44rem] w-[14.79rem] h-[12.84rem] object-cover"
-              alt=""
-              src="/rectangle-30@2x.png"
-            />
-          </div>
-          <div className="absolute top-[29.81rem] left-[10rem] shadow-[0px_4px_4px_rgba(0,_0,_0,_0.25)] w-[70rem] h-[17.69rem]">
-            <div className="absolute top-[0rem] left-[0rem] bg-lightgreen-300 w-[16.33rem] h-[17.69rem]" />
-            <div className="absolute top-[0rem] left-[17.87rem] bg-lightgreen-300 w-[16.39rem] h-[17.69rem]" />
-            <img
-              className="absolute top-[0.53rem] left-[0.89rem] w-[14.79rem] h-[12.84rem] object-cover"
-              alt=""
-              src="/rectangle-26@2x.png"
-            />
-            <div className="absolute top-[0rem] left-[35.8rem] bg-lightgreen-300 w-[16.33rem] h-[17.69rem]" />
-            <div className="absolute top-[0rem] left-[53.67rem] bg-lightgreen-300 w-[16.33rem] h-[17.69rem]" />
-            <img
-              className="absolute top-[0.53rem] left-[18.82rem] w-[14.79rem] h-[12.84rem] object-cover"
-              alt=""
-              src="/rectangle-27@2x.png"
-            />
-            <img
-              className="absolute top-[0.53rem] left-[37.22rem] w-[14.79rem] h-[12.84rem] object-cover"
-              alt=""
-              src="/rectangle-28@2x.png"
-            />
-            <img
-              className="absolute top-[0.53rem] left-[54.44rem] w-[14.79rem] h-[12.84rem] object-cover"
-              alt=""
-              src="/rectangle-30@2x.png"
-            />
-          </div>
-        </div>
-        <div className="absolute top-[0.94rem] left-[10.06rem] w-[9.25rem] flex flex-row items-center justify-start gap-[60.13rem] text-gray-100">
-          <div className="w-[6.44rem] relative h-[2.5rem]">
-            <div className="absolute top-[0%] left-[0%] leading-[100%]">
-              <span>Unity</span>
-              <span className="text-olivedrab">Share</span>
-            </div>
-          </div>
-          <b className="w-[3.44rem] relative text-[1.25rem] leading-[100%] hidden font-web-para-reg-03 text-olivedrab">
-            Login
-          </b>
+const [getItem,setGetItem] = useState([])
+const navigation = useNavigate()
+    useEffect(()=>{
+    const getHandler =async()=>{
+try{
+    const response = await axios.get("https://auth-api-jexl.onrender.com/category/your-model")
+    if(response.data){
+        console.log("dd")
+       console.log(response)
+        setGetItem(response.data)
+    }
+}
+  catch(error){
+    
+  }  
+    }
+    getHandler();
+    },[])
+  return (
+    <div className=" flex flex-col">
+      <div className="w-[100vw] h-[80px] bg-[#E8F6D1] pl-[161px] flex justify-start pt-[10px] gap-[11.04vw]">
+        <Logo />
+        <div className=" h-[54px] w-[39.16vw] bg-[#BFE68C] rounded-[50px]">
+          <input
+            className=" bg-transparent h-[100%] w-[100%] text-[24px] text-[#a7d56b] px-[15px] "
+            placeholder="Search here"
+          />
         </div>
       </div>
-    );
-  };
-  
-  export default ListPage;
-  
+      <div className="to w-[100vw] h-[66px] px-[160px] flex justify-between py-[20px] ">
+        <div className=" cursor-pointer " onClick={()=>navigation("/home")}>Back</div>
+        <div>
+            <select className="bg-white p-2 rounded-md">
+              <option value="category1">Category 1</option>
+              <option value="category2">Category 2</option>
+              <option value="category3">Category 3</option>
+             
+            </select>
+          </div>
+      </div>
+      <div className=" mt-[10px] mx-auto flex flex-col gap-[26.92px]">
+        <div className=" flex gap-[24.62px]">
+        {getItem?getItem.map((item,index)=>(
+            index <= 3?
+            <div className="to h-[283px] w-[18.15vw] flex flex-col justify-between px-[10px] py-[10px] pb-[20px]">
+            <img src={item.image_field} className="w-[100%] h-[205px]" />
+            <div className=" text-[#98c361] text-[1.5rem]">Brown Sofa</div>
+          </div>:null
+        )):null}
+          
+         
+        </div>
+        <div className=" flex gap-[24.62px]">
+        {getItem?getItem.map((item,index)=>(
+            index >= 3 && index<=6?
+            <div className="to h-[283px] w-[18.15vw] flex flex-col justify-between px-[10px] py-[10px] pb-[20px]">
+            <img src={item.image_field} className="w-[100%] h-[205px]" />
+            <div className=" text-[#98c361] text-[1.5rem]">Brown Sofa</div>
+          </div>:null
+        )):null}
+          
+         
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ListPage;
